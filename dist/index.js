2da61a302afe0b53e804937cedfd0beae9f89200
IT052310 <it052310@IT052310s-MacBook-Pro.local>
2023-07-17 22:28:18 -0700

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("Hello World!");
+const thisIsMyFunction = (myName) => {
+    const myVar = "I Will Say Hi";
+    return myVar + " And Hello There! " + myName;
+};
+console.log(thisIsMyFunction("Joy"));
+const sayMyName = () => {
+    console.log("Hello There");
+};
+sayMyName();
+let helloWorld = "Hello World!";
+console.log(helloWorld);
---
+++ index.ts
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+
+const thisIsMyFunction = (myName: String): String => {
+  const myVar = "I Will Say Hi";
+  return myVar + " And Hello There! " + myName;
+};
+
+console.log(thisIsMyFunction("Joy"));
+
+const sayMyName = () => {
+  console.log("Hello There");
+};
+
+sayMyName();
+
+let helloWorld: String = "Hello World!";
+
+console.log(helloWorld);
